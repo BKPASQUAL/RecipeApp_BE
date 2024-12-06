43a86296d18f2dd7984eff728562ec7be0fe75c7
Bawantha <bawanthapasqual@gmail.com>
2024-12-06 12:29:31 +0530

    Set Up

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const cors = require("cors");
+const { connectToDatabase } = require("./config/db.config");
+const routes = require("./routes/index.routes");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 4000;
+
+app.use(cors());
+app.use(express.json());
+
+connectToDatabase();
+
+app.use("/", routes);
+
+app.listen(port, () => {
+  console.log(`SERVER RUNNING ON PORT ${port}`);
+});
